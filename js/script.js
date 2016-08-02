976d9d128134582a268e8819a844bae48458857b
Brandi Williams <brandimichelle.0911@gmail.com>
2016-08-02 13:47:12 -0700

    Fixed links again

--- js/script.js
+++ js/script.js
@@ -99,7 +99,11 @@ PhaserGame.prototype = {
     },
     preload: function () {
         //Preload the sprite images
-        this.load.baseURL = '/PracticeShooter';
+        //For live
+        this.load.baseURL = '/PracticeShooter/';
+
+        //For dev
+        //this.load.baseURL = '/';
         this.load.image('background', 'img/cool-space-background2.jpg');
         this.load.image('player', 'img/ship1.png');
         this.load.image('missile', 'img/missile.png');
